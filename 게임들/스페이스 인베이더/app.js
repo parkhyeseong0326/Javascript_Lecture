af978a1d71d84fd5b12abe7f49d91ddabac73194
parkhyeseong <2017cococo@gmail.com>
2024-06-24 13:16:31 +0900

    space invader

---
+++ 게임들/스페이스 인베이더/app.js
@@ -0,0 +1,12 @@
+const stageSize = 15;
+
+for(let i=0; i < stageSize*stageSize; i++) {
+    const box = document.createElement("div");
+    document.querySelector(".grid").appendChild(box);
+}
+
+const grid = document.querySelector(".grid");
+const stage = document.querySelectorAll(".grid div");
+const startBtn = document.querySelector(".startBtn");
+const stopBtn = document.querySelector(".stopBtn");
+const display = document.querySelector(".display");
